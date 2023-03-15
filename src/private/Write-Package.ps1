bae475855392ce163e558ff7780311113e60116b
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-03-15 12:27:47 -0500

    Fix slowness when adding source (#7)

--- src/private/Write-Package.ps1
+++ src/private/Write-Package.ps1
@@ -9,13 +9,17 @@ function Write-Package {
 		$Request = $Request
 	)
 
+	begin {
+		$sources = Cobalt\Get-WinGetSource
+	}
+
 	process {
 		foreach ($package in $InputObject) {
 			if ($package.ID -and $package.Version) {
 				# WinGet can sometimes return packages without version information, which AnyPackage doesn't like
 				if ($package.Source) {
 					# If source information is provided, construct a source object for inclusion in the results
-					$source = $Request.NewSourceInfo($package.Source,(Cobalt\Get-WinGetSource | Where-Object Name -EQ $package.Source | Select-Object -ExpandProperty Arg),$true)
+					$source = $Request.NewSourceInfo($package.Source,($sources | Where-Object Name -EQ $package.Source | Select-Object -ExpandProperty Arg),$true)
 					$Request.WritePackage($package.ID, $package.Version, '', $source)
 				} else {
 					$Request.WritePackage($package.ID, $package.Version)
