68eaf904343a2bb06c4dcd249effd4c81c42fd3c
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-03-15 20:36:59 -0500

    Fix missing packages with no version number

--- src/private/Write-Package.ps1
+++ src/private/Write-Package.ps1
@@ -15,8 +15,12 @@ function Write-Package {
 
 	process {
 		foreach ($package in $InputObject) {
-			if ($package.ID -and $package.Version) {
-				# WinGet can sometimes return packages without version information, which AnyPackage doesn't like
+			if ($package.ID) {
+				if (-not $package.Version) {
+					$Request.WriteVerbose("Package '$($package.ID)' does not have a version, changing to 0.")
+					$package.Version = '0'
+				}
+
 				if ($package.Source) {
 					# If source information is provided, construct a source object for inclusion in the results
 					$source = $Request.NewSourceInfo($package.Source,($sources | Where-Object Name -EQ $package.Source | Select-Object -ExpandProperty Arg),$true)
