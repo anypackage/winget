6eb91fcb79c79af3b09dc03f3100d044313c6570
Thomas Nieto <38873752+ThomasNieto@users.noreply.github.com>
2023-04-06 00:35:34 -0500

    Fixes

--- src/private/Write-Package.ps1
+++ src/private/Write-Package.ps1
@@ -16,18 +16,17 @@ function Write-Package {
 	process {
 		foreach ($package in $InputObject) {
 			if ($package.ID) {
+				if (-not $package.version) {
+					$package.Version = $null
+				}
+				
 				if ($package.Source) {
 					# If source information is provided, construct a source object for inclusion in the results
 					$location = $sources | Where-Object Name -EQ $package.Source | Select-Object -ExpandProperty Arg
 					$source = [PackageSourceInfo]::new($package.Source, $location, $true, $Request.ProviderInfo)
-				} else {
-					$source = $null
-				}
-
-				if ($package.Version) {
 					$package = [PackageInfo]::new($package.ID, $package.Version, $source, $Request.ProviderInfo)
 				} else {
-					$package = [PackageInfo]::new($package.ID, $null, $source, $Request.ProviderInfo)
+					$package = [PackageInfo]::new($package.ID, $package.Version, $Request.ProviderInfo)
 				}
 
 				$Request.WritePackage($package)
